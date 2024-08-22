365e84a3e77635c94c31f01f09a3ac052fac5465
ChisThanh <ChisThanh@CHISTHANH>
2024-08-22 18:10:02 +0700

    Add project files.

---
+++ EvermoreBakery/Service/Users.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EvermoreBakery.Service
+{
+    [Table("users")]
+    public class Users
+    {
+        [Key]
+        public int Id { set; get; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { set; get; }
+    }
+}
